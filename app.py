22e6d65f023a250294df4daf411fcac71c784d98
Katie Gamanji <gamanjie@gmail.com>
2020-10-31 17:38:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ if __name__ == "__main__":
     ## stream logs to a file
     logging.basicConfig(filename='app.log',level=logging.DEBUG)
     
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8111)
